3a09e9c9217446cd27049ec914c1feea3213c6e9
viniciuspinho <vinipinho@gmail.com>
2026-03-28 21:28:58 +0000

    teste deploy automatico outra vez

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Nova versão deploy automático 🔥"
+    return "Nova versão deploy automático de novo 🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
