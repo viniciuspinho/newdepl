708a52913fbe599bfeb3dd823cb118858e731af5
viniciuspinho <vinipinho@gmail.com>
2026-03-28 22:03:45 +0000

    altera texto do app para testar rollback

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Nova versão deploy automático de novo com notificação de e-mail e rollback🔥"
+    return "Versão nova em produção - teste de rollback"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
