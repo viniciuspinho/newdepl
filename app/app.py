dff4e64fcf789d9b76cb898d4e940c729c10170d
viniciuspinho <vinipinho@gmail.com>
2026-03-28 21:55:53 +0000

    Minha alteração

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Nova versão deploy automático de novo com notificação de e-mail🔥"
+    return "Nova versão deploy automático de novo com notificação de e-mail e rollback🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
