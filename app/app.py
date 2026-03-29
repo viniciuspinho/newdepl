27d390db71bbca71b9bd2392ebd35f0c069f4597
viniciuspinho <vinipinho@gmail.com>
2026-03-29 00:34:35 +0000

    altera texto do app para teste com tag

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Versão nova em produção - teste de rollback"
+    return "Nova versão com tag"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
